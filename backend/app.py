8fcd5d052efe7d17241fd8b65555ecda60e60e66
kevanscis <kevan.teo.2022@scis.smu.edu.sg>
2026-02-06 16:50:42 +0800

    .

--- backend/app.py
+++ backend/app.py
@@ -53,11 +53,21 @@ def classify_question(question):
 
 @app.after_request
 def add_cors_headers(response):
-    """Add CORS headers to every response"""
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-    response.headers['Access-Control-Max-Age'] = '86400'
+    allowed = {
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:5000",
+        "http://127.0.0.1:5000",
+    }
+
+    origin = request.headers.get("Origin")
+    if origin in allowed:
+        response.headers["Access-Control-Allow-Origin"] = origin
+
+    response.headers["Vary"] = "Origin"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    response.headers["Access-Control-Max-Age"] = "86400"
     return response
 
 @app.route('/api/questions', methods=['OPTIONS'])
